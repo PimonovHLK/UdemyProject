0ea1baa303238b56d07a191215313880b1cec832
PimonovHLK <pimonondm@gmail.com>
2021-03-31 17:58:19 +0400

    edit lesson js

--- js/script.js
+++ js/script.js
@@ -8,12 +8,29 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('один из последних просмотренных фильмов', ''),
-      b = prompt('на сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
-      console.log(personalMovieDB);
-      
+for (let i = 0; i < 2; i++) {
+    const a = prompt('один из последних просмотренных фильмов', ''),
+          b = prompt('на сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+    // personalMovieDB.movies[a] = b;
+}
+
+if(personalMovieDB.count < 10) {
+    console.log("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("ошмбка");
+}
+
+console.log(personalMovieDB);      
