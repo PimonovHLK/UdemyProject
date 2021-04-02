08c07d10b167c849ddcdfe12b0f0570a4eca9f21
PimonovHLK <pimonondm@gmail.com>
2021-04-02 11:09:45 +0400

    lesson 19

--- js/script.js
+++ js/script.js
@@ -1,68 +1,11 @@
 "use strict";
-
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('сколько фильмов уже смотрели', '');
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('сколько фильмов уже смотрели', '');
-    }
+function learnJS(lang, callback) {
+    console.log(`я учу${lang}`);
+    callback();
 }
 
-// start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('один из последних просмотренных фильмов', ''),
-              b = prompt('на сколько оцените его?', '');
-        if (a != null && b != null && a != '' && b != '' && a.length < 50){
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    
-        // personalMovieDB.movies[a] = b;
-    }
+function done() {
+    console.log('я прошел это урок!');
 }
 
-// rememberMyFilms();
-
-function detectPersonalLevel() {
-    if(personalMovieDB.count < 10) {
-        console.log("Просмотренно довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count > 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("ошмбка");
-    }
-    
-}
-
-// detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden){
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
- 
-function writeYourGeneral() {
-    for (let i = 1; i <= 3; i++) {        
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-writeYourGeneral();
+learnJS('JavaScript', done);
