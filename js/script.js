571d6b861f540d335c77a7e5b49b9eeffe564db6
PimonovHLK <pimonondm@gmail.com>
2021-04-02 12:58:16 +0400

    lesson 20

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
 "use strict";
-function learnJS(lang, callback) {
-    console.log(`я учу${lang}`);
-    callback();
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function(){
+        console.log("Test");
+    }
+    
+};
 
-function done() {
-    console.log('я прошел это урок!');
-}
+options.makeTest();
 
-learnJS('JavaScript', done);
+const {border, bg} = options.colors;
+console.log(border);
+// delete(options.name);
+// let counter = 0;
+
+// for (let key in options){
+//     if(typeof(options[key]) === 'object') {
+//         for(let i in options[key]){
+//             console.log(`Свойство ${i} значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} значение ${options[key]}`);
+//         counter++;    
+//     }        
+// }
+
+console.log(Object.keys(options).length);
