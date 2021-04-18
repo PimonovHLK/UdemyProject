40d528bb02b4b38b486ea72a7a07690c86034d51
PimonovHLK <pimonondm@gmail.com>
2021-04-18 20:46:47 +0400

    lesson 21

--- js/script.js
+++ js/script.js
@@ -1,35 +1,9 @@
 "use strict";
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function(){
-        console.log("Test");
-    }
-    
-};
 
-options.makeTest();
+const arr = [2, 3, 48, 5, 8];
+arr.sort(compareNum);
+console.log(arr);
 
-const {border, bg} = options.colors;
-console.log(border);
-// delete(options.name);
-// let counter = 0;
-
-// for (let key in options){
-//     if(typeof(options[key]) === 'object') {
-//         for(let i in options[key]){
-//             console.log(`Свойство ${i} значение ${options[key][i]}`);
-//             counter++;
-//         }
-//     } else {
-//         console.log(`Свойство ${key} значение ${options[key]}`);
-//         counter++;    
-//     }        
-// }
-
-console.log(Object.keys(options).length);
+function compareNum(a, b){
+    return a - b;
+}
