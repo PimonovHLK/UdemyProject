15155d77b4f0373e5537bb9da5d490f6bbb2733b
PimonovHLK <pimonondm@gmail.com>
2021-07-07 00:05:08 +0400

    lesson 41

--- Food_less38/js/script.js
+++ Food_less38/js/script.js
@@ -34,4 +34,57 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // Timer 
+
+    const dedline = '2021-07-08';
+
+    function getTimerRemaining(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+            
+        return {
+            'total': t,
+            'days' : days,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+            updateClock();
+            function updateClock(){
+                const t = getTimerRemaining(endtime);
+                
+                days.innerText = getZero(t.days);
+                hours.innerText = getZero(t.hours);
+                minutes.innerText = getZero(t.minutes);
+                seconds.innerText = getZero(t.seconds);
+
+                if (t.total <= 0) {
+                    clearInterval(timeInterval);
+                }
+            }
+    }
+
+    setClock('.timer', dedline);
 });
