5b0f315e64453cebdfdaadad23941785ac3baadf
PimonovHLK <pimonondm@gmail.com>
2021-08-05 22:07:08 +0400

    less48

--- Food_less38/js/script.js
+++ Food_less38/js/script.js
@@ -122,7 +122,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 10000);
+    // const modalTimerId = setTimeout(openModal, 10000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight > document.documentElement.scrollHeight) {
@@ -133,25 +133,89 @@ window.addEventListener('DOMContentLoaded', () => {
     
     window.addEventListener('scroll', showModalByScroll);
 
-    function User(name, id) {
-        this.name = name;
-        this.id = id;
-        this.human = true;
-        this.hello = function () {
-            console.log(`hello ${this.name}`);
-        };
+    // function User(name, id) {
+    //     this.name = name;
+    //     this.id = id;
+    //     this.human = true;
+    //     this.hello = function () {
+    //         console.log(`hello ${this.name}`);
+    //     };
+    // }
+
+    // User.prototype.exit = function (name) {
+    //     console.log(`Пользователь ${this.name} ушел в закат`);
+    // };
+
+    // const ivan = new User('Ivan', 28);
+    // const alex = new User('Alex', 20);
+
+    // ivan.exit();
+    // alex.hello();
+
+    // console.log(ivan);
+    // console.log(alex);
+
+    // используем классы для карточек
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+        
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+             </div>
+            `;
+            this.parent.append(element);
+        }
     }
 
-    User.prototype.exit = function (name) {
-        console.log(`Пользователь ${this.name} ушел в закат`);
-    };
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        "Меню 'Фитнес'",
+        '"Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+    
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        '"Меню "Премиум" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        16,
+        '.menu .container'
+    ).render();
 
-    const ivan = new User('Ivan', 28);
-    const alex = new User('Alex', 20);
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню Постное',
+        '"Меню "Постное" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        14,
+        '.menu .container'
+    ).render();
 
-    ivan.exit();
-    alex.hello();
-
-    console.log(ivan);
-    console.log(alex);
 });
