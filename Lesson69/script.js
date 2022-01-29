e34de4ef8e70b4e594ab420e76bbe349b664947f
PimonovHLK <pimonondm@gmail.com>
2022-01-29 17:48:09 +0400

    less69

---
+++ Lesson69/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+    }
+
+    #surname = 'Petrychenko';
+
+    say = () => {
+        console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст ${this._age}`);
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('Недопустимое значение!');
+        }
+    }
+
+    get surname() {
+        return this.#surname;
+    }
+     set surname(name) {
+         this.#surname = name;
+     }
+
+}
+
+const ivan = new User('Ivan', 27);
+
+console.log(ivan.surname);
+
+console.log(ivan.surname = "Pimonov");
