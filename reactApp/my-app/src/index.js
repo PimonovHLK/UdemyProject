d06189ee7ef73a9cd3789a4752ca865f9df1fa07
PimonovHLK <pimonondm@gmail.com>
2022-02-27 16:48:58 +0400

    less88

--- reactApp/my-app/src/index.js
+++ reactApp/my-app/src/index.js
@@ -1,21 +1,10 @@
-import React, { createElement } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-const text = 'Hello world';
-
-const elem = (
-  <div>
-    <h2 className='text'>Текст: {text}</h2>
-    <input type="text" />
-    <label htmlFor=""></label>
-    <button tabIndex="0">Click</button>
-  </div>
-);
-
-// const elem = React.createElement('h2', {className: 'greeting'}, 'Hello world');
+import {Header} from './App';
 
 ReactDOM.render(
-  elem,
+  <App/>,
   document.getElementById('root')
 );
