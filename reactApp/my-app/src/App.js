5e7889e994bdd115cfbd8c269f9fecbae7c81f5d
PimonovHLK <pimonondm@gmail.com>
2022-03-14 08:28:52 +0400

    less93

--- reactApp/my-app/src/App.js
+++ reactApp/my-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import { Component, StrictMode } from 'react';
 
 const Header = () => {
   return <h2>Hello world!</h2>
@@ -36,7 +36,9 @@ function Btn() {
 function App() {
   return (
     <div className="App">
-      <Header/>
+     <StrictMode>
+        <Header/>
+     </StrictMode>
       <Field/>
       <Btn/>
     </div>
