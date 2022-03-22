337f2ee6a46b69adb31539288413ce17a195b642
Дмитрий <d.pimonov@promo-z.ru>
2022-03-22 17:39:14 +0400

    lesson98

--- EMPLOYERS/src/components/app/app.js
+++ EMPLOYERS/src/components/app/app.js
@@ -1,79 +1,80 @@
-// import AppInfo from '../app-info/app-info';
-// import SearchPanel from '../search-panel/search-panel';
-// import AppFilter from '../app-filter/app-filter';
-// import EmployeesList from '../employees-list/employees-list';
-// import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import { Component } from 'react';
 
 import './app.css';
 
-class WhoAmi extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      years: 27,
-      position: ''
-    }
-    // this.nextYear = this.nextYear.bind(this);
-  }
+// class WhoAmi extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       years: 27,
+//       position: ''
+//     }
+//     // this.nextYear = this.nextYear.bind(this);
+//   }
 
-nextYear = () => {  
-  this.setState(state => ({
-    years: state.years + 1
-  }))
-}
-commitInputChanges = (e, color) => {
-  console.log(color);
-  this.setState({
-    position: e.target.value
-  })
-}
+// nextYear = () => {  
+//   this.setState(state => ({
+//     years: state.years + 1
+//   }))
+// }
+// commitInputChanges = (e, color) => {
+//   console.log(color);
+//   this.setState({
+//     position: e.target.value
+//   })
+// }
 
-  render() {
-    const {name, surname, link} = this.props;
-    const {position, years} = this.state;
-    return (
-      <div>
-        <button onClick={this.nextYear}>{this.state.text}</button>
-        <h1>My name is {name}, surname - {surname},
-         age - {years}, 
-        position - {position}</h1>
-        <a href={link}>My profile</a>
-        <form>
-          <span>Введите должность</span>
-          <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
-        </form>
-      </div>
-    )
-  }
-}
+//   render() {
+//     const {name, surname, link} = this.props;
+//     const {position, years} = this.state;
+//     return (
+//       <div>
+//         <button onClick={this.nextYear}>{this.state.text}</button>
+//         <h1>My name is {name}, surname - {surname},
+//          age - {years}, 
+//         position - {position}</h1>
+//         <a href={link}>My profile</a>
+//         <form>
+//           <span>Введите должность</span>
+//           <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+//         </form>
+//       </div>
+//     )
+//   }
+// }
 
 function App() {
   
-  return (
-    <div className="App">
-      <WhoAmi name='John' surname="Smith" link="facebook.com"/>
-      <WhoAmi name='Alex' surname="Shepard" link="vk.com"/>    
-    </div>
-  )
-
-  // const data = [
-  //   {name: 'John C.', salary: 800, increase: false, id: 1}, 
-  //   {name: 'Alex M.', salary: 3000, increase: true, id: 2}, 
-  //   {name: 'Carl W.', salary: 15000, increase: false, id: 3}, 
-  // ]
-
   // return (
-  //   <div className="app">        
-  //       <AppInfo />
-  //       <div className="search-panel">
-  //           <SearchPanel/>
-  //           <AppFilter/>
-  //       </div>        
-  //       <EmployeesList data={data}/>
-  //       <EmployeesAddForm/>
+  //   <div className="App">
+  //     <WhoAmi name='John' surname="Smith" link="facebook.com"/>
+  //     <WhoAmi name='Alex' surname="Shepard" link="vk.com"/>    
   //   </div>
-  // );
+  // )
+
+  const data = [
+    {name: 'John C.', salary: 800, increase: false, id: 1}, 
+    {name: 'Alex M.', salary: 3000, increase: true, id: 2}, 
+    {name: 'Carl W.', salary: 15000, increase: false, id: 3}, 
+  ]
+
+  return (
+    <div className="app">        
+        <AppInfo />
+        <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+            <AppFilter/>
+        </div>        
+        <EmployeesList data={data}/>
+        <EmployeesAddForm/>
+    </div>
+  );
 }
 
 export default App;
