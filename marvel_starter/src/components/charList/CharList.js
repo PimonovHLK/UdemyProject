17a9e09a0d0b8967c45d9c08dfa83296058890d9
PimonovHLK <pimonondm@gmail.com>
2022-05-06 17:57:11 +0400

    Lesson159

--- marvel_starter/src/components/charList/CharList.js
+++ marvel_starter/src/components/charList/CharList.js
@@ -13,7 +13,7 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 1548,
+        offset: 210,
         charEnded: false
 
     }
@@ -64,16 +64,37 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = []
+    setRefs = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+
+    }
+
     renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
             }
             return (
                 <li className="char__item"
+                ref={this.setRefs}
                 key={item.id}
-                onClick={() => this.props.onCharSelected(item.id)}>
+                onClick={() => {this.props.onCharSelected(item.id);
+                                this.focusOnItem(i);
+                                }}
+                onKeyPress={(e) => {
+                    if (e.key === ' ' || e.key === "Enter") {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }
+                }}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
                 </li>
