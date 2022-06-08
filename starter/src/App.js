e2d9b30e3e8afe4935f7bb8673da70b47e5f9b0d
PimonovHLK <pimonondm@gmail.com>
2022-06-08 10:08:40 +0400

    Lesson 178

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,132 +1,79 @@
-import {Component, useState, useEffect} from 'react';
+import {useState, memo, useCallback} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
-// class Slider extends Component {
 
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             autoplay: false,
-//             slide: 0
-//         }
-//     }
+// function propsCompare(prevProps, nextProps) {
+//     return prevProps.mail.name === nextProps.mail.name;
+// }
 
-//     componentDidMount(){
-//         document.title = `Slide: ${this.state.slide}`;
-//     }
+// class Form extends Component {
 
-//     componentDidUpdate() {
-//         document.title = `Slide: ${this.state.slide}`;
-//     }
-
-//     changeSlide = (i) => {
-//         this.setState(({slide}) => ({
-//             slide: slide + i
-//         }))
-//     }
-
-//     toggleAutoplay = () => {
-//         this.setState(({autoplay}) => ({
-//             autoplay: !autoplay
-//         }))
+//     shouldComponentUpdate(nextProps) {
+//         if(this.props.mail.name === nextProps.mail.name) {
+//             return false
+//         } return true
 //     }
 
 //     render() {
+//         console.log('render');
 //         return (
 //             <Container>
-//                 <div className="slider w-50 m-auto">
-//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-//                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-//                     <div className="buttons mt-3">
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(-1)}>-1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(1)}>+1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={this.toggleAutoplay}>toggle autoplay</button>
+//                 <form className="w-50 border mt-5 p-3 m-auto">
+//                     <div className="mb-3">
+//                         <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+//                         <input value={this.props.mail.name} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com"/>
+//                         </div>
+//                         <div className="mb-3">
+//                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+//                         <textarea value={this.props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 //                     </div>
-//                 </div>
+//                 </form>
 //             </Container>
 //         )
 //     }
 // }
 
-const calcValue = () => {
-    console.log('random');
-
-    return Math.random() * (50-1) + 1
-}
-
-const Slider = (props) => {
-
-    const [slide, setSlide] = useState(0);
-    const [autoplay, setAutoplay] = useState(false);
-
-    function logging() {
-        console.log('log!');
-    }
-
-    useEffect(() => {
-        console.log('effect');
-        document.title = `Slide: ${slide}`;
-
-        window.addEventListener('click', logging);
-
-        return () => {
-            window.removeEventListener('click', logging);
-        }
-
-    }, [slide]);
-
-    useEffect(() => {
-        console.log('autoplay');
-    }, [autoplay]);
-
-    function changeSlide(i) {
-        setSlide(slide => slide + i); 
-    }
-
-    function toggleAutoplay() {
-        setAutoplay(autoplay => !autoplay);
-    }
-
+const Form = memo((props) => {
+    console.log('render');
     return (
         <Container>
-            <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null} </div>
-                <div className="buttons mt-3">
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => changeSlide(-1)}>-1</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => changeSlide(1)}>+1</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={toggleAutoplay}>toggle autoplay</button>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+                    <input value={props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea value={props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
-            </div>
+            </form>
         </Container>
     )
-}
-
+})
 
 function App() {
+    const [data, setData] = useState({
 
-    const[slider, setSlider] = useState(true);
+        mail: "name@example.com",
+        text: 'some text'
+    });
 
-  return (
-        <>  
-            <button onClick={() => setSlider(false)}>Click</button>
-            {slider ? <Slider/> : null}
-            
+    const onLog = useCallback(() => {
+        console.log('wwow');
+    }, [])
+
+    return (
+        <>
+            <Form mail={data.mail} text={data.text} onLog={onLog}/>
+            <button 
+                onClick={() => setData({
+                    mail: "name@example.com",
+                    text: 'some text'
+                })}>
+                Click me
+            </button>
         </>
-        
-  );
+    );
 }
 
 export default App;
